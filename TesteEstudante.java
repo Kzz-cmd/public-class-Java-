f861a61558219f979b40636b7a32c2569f47c56e
Kenzzo <kenzzo123f22@gmail.com>
2026-02-06 18:53:07 -0300

    Delete TesteEstudante.java

--- TesteEstudante.java
+++
@@ -1,8 +0,0 @@
-public class TesteEstudante {
-    public static void main(String[] args) {
-        Estudante a1 = new Estudante("Antônio Carlos Peba da Silva", new double[]{7.5,5.0,8.75,10.0});
-        System.out.println("Nome do Aluno: " + a1.nome + "\nSuas notas:");
-        a1.exibirNotas();
-        System.out.printf("Sua Média: %.2f",a1.calcularMedia());
-    }
-}
